28f4745f99fc36e97e0043331d45e3e88e45380e
Malene Thisgaard <malene@whiterabbitjapan.com>
2023-12-09 21:43:49 +0900

    Adjust index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="flex gap-12">
+  <div class="flex flex-col gap-12 md:mt-12 lg:gap-24 lg:flex-row">
     <div>
-      <h1 class="mb-6 text-4xl font-bold">Hi there, I'm Malene</h1>
+      <img
+        class="rounded-xl md:max-w-[400px]"
+        src="~/assets/images/avatar.jpg"
+        alt="Malene"
+      />
+    </div>
+    <div>
+      <h1>Hi there, I'm Malene 👋</h1>
       <div>
-        I'm a Full-Stack Web Developer with a knack for creating user-friendly
-        web experiences. Combining my coding skills with my psychology and
-        behavioral design background, I love turning complex projects into
-        smooth and enjoyable UX. Managing end-to-end project lifecycles, from
-        scoping to coding and testing, I ensure that every part of the project
-        gets the attention it needs. Let's build something great together!
+        I'm a full stack engineer who likes to think outside the browser. With a
+        background in psychology and a love for clean, user-friendly design, I
+        make websites that are as engaging as they are functional. From the
+        drawing board to deployment, I enjoy managing the entire life cycle of a
+        project.
+      </div>
+      <div class="mt-2">
+        I build great products by working iteratively with stakeholders and
+        fellow engineers to identify the best approach, starting with a simple
+        MVP, and continuously improving through trial and error. I thrive on
+        tackling complex problems, learning from each try, and experimenting
+        with new ideas along the way.
       </div>
       <div class="flex gap-6 text-[24px] mt-6 text-gray-400">
         <a
@@ -33,13 +46,6 @@
           <FA :icon="['far', 'envelope']" />
         </a>
       </div>
-    </div>
-    <div>
-      <img
-        class="rounded-xl max-w-[400px]"
-        src="~/assets/images/avatar.jpg"
-        alt="Malene"
-      />
     </div>
   </div>
 </template>
