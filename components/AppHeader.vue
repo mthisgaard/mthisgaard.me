365aecd59eaacd15fca84e5eeced34c420f32541
Malene Thisgaard <malene@whiterabbitjapan.com>
2023-12-09 21:43:34 +0900

    Add resume download to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,15 +2,22 @@
   <header class="flex justify-between mt-8 mb-16">
     <NuxtLink class="text-2xl font-bold" to="/"> MT </NuxtLink>
     <nav>
-      <ul class="flex gap-4">
-        <li>
-          <NuxtLink to="/about"> About </NuxtLink>
-        </li>
+      <ul class="flex gap-6">
         <li>
           <NuxtLink to="/skills"> Skills </NuxtLink>
         </li>
         <li>
           <NuxtLink to="/projects"> Projects </NuxtLink>
+        </li>
+        <li>
+          <a
+            href="/resume_malene_thisgaard.pdf"
+            download
+            class="px-4 py-2 duration-500 transform bg-gray-300 rounded-lg opacity-80 hover:opacity-100 hover:-translate-y-1"
+          >
+            <span class="mr-2">Resume</span>
+            <FA :icon="['fas', 'download']" />
+          </a>
         </li>
       </ul>
     </nav>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -3,11 +3,11 @@ import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-library.add(faEnvelope, faGithub, faLinkedin, faAngleDown)
+library.add(faEnvelope, faGithub, faLinkedin, faDownload)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('FA', FontAwesomeIcon)
