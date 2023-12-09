8892f683af94b884817a57845e1d92a196a216ba
Malene Thisgaard <malene@whiterabbitjapan.com>
2023-12-09 13:09:18 +0900

    Setup Page titles and meta data

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+useHead({
+	title: 'Portfolio | Malene Thisgaard',
+	meta: [
+		{ name: 'description', content: 'Portfolio site for Malene Thisgaard, Full Stack Engineer based in Tokyo Japan.' }
+	],
+})
+useSeoMeta({
+	ogTitle: 'Malene Thisgaard | Portfolio',
+	description: 'Portfolio site for Malene Thisgaard, Full Stack Engineer based in Tokyo Japan.',
+	ogDescription: 'Portfolio site for Malene Thisgaard, Full Stack Engineer based in Tokyo Japan.',
+	ogImage: '',
+})
+</script>
+
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <NuxtPage />
 </template>
--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+useHead({
+	title: 'About | Malene Thisgaard',
+})
+</script>
 <template>
   <section>
     <p>This page will be displayed at the /about route.</p>
